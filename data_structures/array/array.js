287cdbd94e0b82571ce134bba7a2e98cdbd0fb2e
Fayez Saadi <fayezosaadi@proton.me>
2025-07-26 13:25:35 -0600

    First commit

---
+++ data_structures/array/array.js
@@ -0,0 +1,36 @@
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length++] = item;
+    }
+
+    pop() {
+        const last = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return last;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+}
